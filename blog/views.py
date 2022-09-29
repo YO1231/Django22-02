8c62c35965122016fed7de64de534311a5f195b8
YO1231 <whee1231@duksung.com>
2022-09-29 13:49:51 +0900

    FBV로 포스트 목록/상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts1 = Post.objects.all().order_by('-pk')
+    return render(request, 'blog/index.html', {'posts2' : posts1}) # 오른쪽은 위에서 준비된 변수값
+
+def single_post_page(request, pk):
+    post2 = Post.objects.get(pk=pk)
+    return render(request, 'blog/single_post_page.html', {'post': post2})
