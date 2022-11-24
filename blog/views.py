83988ae8b8bbbc0d398ebc5500c0178f089363e4
YO1231 <whee1231@duksung.com>
2022-11-24 13:47:20 +0900

    여러 페이지에 나누어 보여주기

--- blog/views.py
+++ blog/views.py
@@ -87,6 +87,7 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5 #페이지 나누어서 보여주는거.. 포스트 개수
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data() # PostList
